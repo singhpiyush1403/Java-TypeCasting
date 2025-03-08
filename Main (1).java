54589c6cd615413f0298c58ac0c72ec31ec918ad
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-08 12:38:56 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,8 @@
+public class WideningExample {
+    public static void main(String[] args) {
+        int num = 10;
+        double d = num;  
+        System.out.println("Integer: " + num);
+        System.out.println("Double: " + d);
+    }
+}
